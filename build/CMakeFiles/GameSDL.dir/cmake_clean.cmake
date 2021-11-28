ece8400490ad5002d336b65028412abbe9075a6c
HeoKiu <duccn8@icloud.com>
2021-11-28 23:59:14 +0300

    Change Cmake version

---
+++ build/CMakeFiles/GameSDL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameSDL.dir/main.cpp.o"
+  "CMakeFiles/GameSDL.dir/MathEquation.cpp.o"
+  "CMakeFiles/GameSDL.dir/drawer.cpp.o"
+  "CMakeFiles/GameSDL.dir/Sound.cpp.o"
+  "GameSDL.pdb"
+  "GameSDL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameSDL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
