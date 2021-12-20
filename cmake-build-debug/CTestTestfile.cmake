006fc851bb8add8c76a8847bc35b2e10c108636e
HeoKiu <47948344+HeoKiu@users.noreply.github.com>
2021-12-20 19:17:30 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/duc/Game_MIPT_SDL2
-# Build directory: /home/duc/Game_MIPT_SDL2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/googletest")
-subdirs("tests")
--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/duc/Game_MIPT_SDL2/tests
-# Build directory: /home/duc/Game_MIPT_SDL2/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/duc/Game_MIPT_SDL2/cmake-build-debug/tests/tests")
